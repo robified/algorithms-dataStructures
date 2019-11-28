649885b997ce382ab5bbb69b7e3e9eef22fd438e
Robin <daorob@gmail.com>
2019-11-27 21:37:06 -0600

    Add palindromes exercise.

--- exercises.js
+++ exercises.js
@@ -24,6 +24,24 @@
 // };
 
 // Solution #3
-function reverse(string) {
-    return string.split('').reduce((reversed, character) => character + reversed, '')
+// function reverse(string) {
+//     return string.split('').reduce((reversed, character) => character + reversed, '')
+// };
+
+
+
+
+
+// Palindromes
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+
 };

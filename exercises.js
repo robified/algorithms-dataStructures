76a4872e6edeeac86cdb5cd69a419fb0a0141f8e
Robin <daorob@gmail.com>
2019-11-27 21:26:41 -0600

    Solve string reversal exercise with solution #1.

--- exercises.js
+++ exercises.js
@@ -3,10 +3,11 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-
+// Solution #1
+function reverse(string) {
+    return string.split('').reverse().join('')
 };

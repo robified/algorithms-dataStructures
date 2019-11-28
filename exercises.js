fca57ffbb5ffbca6df04c58a06db7016194ff7c5
Robin <daorob@gmail.com>
2019-11-27 22:36:19 -0600

    Solve fizzbuzz exercise.

--- exercises.js
+++ exercises.js
@@ -124,6 +124,16 @@ function maxChar(string) {
 //   4
 //   buzz
 
-function fizzBuzz(n) {
-
+function fizzBuzz(number) {
+    for (let i = 1; i <= number; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else {
+            console.log(i);
+        };
+    };
 };

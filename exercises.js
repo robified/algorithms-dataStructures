7971cb550150a5f5a0711e93db6b4c31a9ae5eb5
Robin <daorob@gmail.com>
2020-01-26 07:27:21 -0600

    Solve queue weaving exercise.

--- exercises.js
+++ exercises.js
@@ -647,19 +647,19 @@ class Queue {
 
 class Queue {
     constructor() {
-      this.data = [];
+        this.data = [];
     }
-  
+
     add(record) {
-      this.data.unshift(record);
+        this.data.unshift(record);
     }
-  
+
     remove() {
-      return this.data.pop();
+        return this.data.pop();
     }
-  
+
     peek() {
-        
+        return this.data[this.data.length - 1];
     }
 }
 
@@ -689,4 +689,18 @@ class Queue {
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue();
+
+    while (sourceOne.peek() || sourceTwo.peek()) {
+        if (sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+
+        if (sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+
+    return q;
+}

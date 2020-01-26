ce40f9cea9a6a4b8c8ebc80747f9fe6af77dec83
Robin <daorob@gmail.com>
2020-01-26 08:15:09 -0600

    Solve stack exercise.

--- exercises.js
+++ exercises.js
@@ -723,5 +723,19 @@ function weave(sourceOne, sourceTwo) {
 //   s.pop(); // returns 1
 
 class Stack {
+    constructor() {
+        this.data = [];
+    }
 
+    push(record) {
+        this.data.push(record);
+    }
+
+    pop() {
+        return this.data.pop();
+    }
+
+    peek() {
+        return this.data[this.data.length - 1];
+    }
 }

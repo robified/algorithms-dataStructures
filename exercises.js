74a761e78158d6bd6b365dcd3bfaff3b7a125598
Robin <daorob@gmail.com>
2020-01-26 08:26:03 -0600

    Add queue from stack exercise.

--- exercises.js
+++ exercises.js
@@ -739,3 +739,28 @@ class Stack {
         return this.data[this.data.length - 1];
     }
 }
+
+
+
+
+
+// Queue From Stack
+// --- Directions
+// Implement a Queue datastructure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back
+// at the Queue exercise.
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+const Stack = require('./stack');
+
+class Queue {
+    
+}

a5ca1dcc33874f0cc48c080a7147a242e6a9f2f9
Robin <daorob@gmail.com>
2020-01-26 10:04:25 -0600

    Solve queue from stack exercise.

--- exercises.js
+++ exercises.js
@@ -762,5 +762,40 @@ class Stack {
 const Stack = require('./stack');
 
 class Queue {
+    constructor() {
+        this.firstStack = new Stack();
+        this.secondStack = new Stack();
+    }
+        
+    add(record) {
+        this.firstStack.push(record);
+    }
     
+    remove() {
+        while (this.firstStack.peek()) {
+            this.secondStack.push(this.firstStack.pop());
+        }
+
+        // this is the record we want
+        const record = this.secondStack.pop();
+        while (this.secondStack.peek()) {
+            this.firstStack.push(this.secondStack.pop());
+        }
+
+        return record;
+    }
+
+    peek() {
+        while (this.firstStack.peek()) {
+            this.secondStack.push(this.firstStack.pop());
+        }
+
+        const record = this.secondStack.peek();
+
+        while (this.secondStack.peek()) {
+            this.firstStack.push(this.secondStack.pop());
+        }
+
+        return record;
+    }   
 }

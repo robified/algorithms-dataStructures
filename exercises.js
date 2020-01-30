54101d56b4eb97896621d1f61c76d3726449d82c
Robin <daorob@gmail.com>
2020-01-30 16:01:39 -0600

    Add insertAt method.

--- exercises.js
+++ exercises.js
@@ -921,7 +921,7 @@ class LinkedList {
             return;
         }
         
-        if (index ===0) {
+        if (index === 0) {
             this.head = this.head.next;
             return;
         }
@@ -931,5 +931,21 @@ class LinkedList {
             return;
         }
         previous.next = previous.next.next;
+    }
+
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const previous = this.getAt(index - 1) || this.getLast();
+        const node = new Node(data, previous.next);
+        previous.next = node;
     }
 }

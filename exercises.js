c0a0b7b56dc1a3a81f59433d4c7d60616227772b
Robin <daorob@gmail.com>
2020-01-30 20:50:32 -0600

    Solve midpoint exercise.

--- exercises.js
+++ exercises.js
@@ -993,5 +993,13 @@ class LinkedList {
 //   midpoint(l); // returns { data: 'b' }
 
 function midpoint(list) {
-    
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while(fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
 }

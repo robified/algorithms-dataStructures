2602c47a1aeb4aff56c94872e05f4843a37ee30c
Robin <daorob@gmail.com>
2020-01-31 01:56:33 -0600

    Add tree width exercise.

--- exercises.js
+++ exercises.js
@@ -1135,3 +1135,25 @@ class Tree {
         }
     }
 }
+
+
+
+
+
+// Tree Width with Level Width
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+function levelWidth(root) {
+    
+}

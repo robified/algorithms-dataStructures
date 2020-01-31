0aa0fb5d96130c5c1b21eeb7d47ed8d0decacaeb
Robin <daorob@gmail.com>
2020-01-31 13:49:23 -0600

    Add events exercise.

--- exercises.js
+++ exercises.js
@@ -1256,3 +1256,26 @@ function validate(node, min = null, max = null) {
 
     return true;
 }
+
+
+
+
+
+// Events
+// --- Directions
+// Create an 'eventing' library out of the
+// Events class.  The Events class should
+// have methods 'on', 'trigger', and 'off'.
+
+class Events {
+    // Register an event handler
+    on(eventName, callback) {}
+  
+    // Trigger all callbacks associated
+    // with a given eventName
+    trigger(eventName) {}
+  
+    // Remove all event handlers associated
+    // with the given eventName
+    off(eventName) {}
+}

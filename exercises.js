e9f9e0aa992651240d33484a2d8b3d1cd9cedf36
Robin <daorob@gmail.com>
2020-01-31 01:10:20 -0600

    Implement remove method to Node class.

--- exercises.js
+++ exercises.js
@@ -1102,6 +1102,12 @@ class Node {
     add(data) {
         this.children.push(new Node(data));
     }
+
+    remove(data) {
+        this.children = this.children.filter(node => {
+            return node.data !== data;
+        });
+    }
 }
 
 class Tree {}

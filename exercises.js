5b2dff3efa71d6b8d6be17eddf2e1f0799f2d5ca
Robin <daorob@gmail.com>
2020-01-31 01:39:02 -0600

    Implement breadth first tree traversal method.

--- exercises.js
+++ exercises.js
@@ -1110,4 +1110,18 @@ class Node {
     }
 }
 
-class Tree {}
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift();
+            
+            arr.push(...node.children);
+            fn(node);
+        }
+    }
+}

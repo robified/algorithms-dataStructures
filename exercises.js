4483ff6b371622813a436b40908ee8dd25a58e10
Robin <daorob@gmail.com>
2020-01-30 23:56:23 -0600

    Solve fromLast exercise.

--- exercises.js
+++ exercises.js
@@ -1060,5 +1060,18 @@ function circular(list) {
 //    fromLast(list, 2).data // 'b'
 
 function fromLast(list, n) {
-    
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (n > 0) {
+        fast = fast.next;
+        n--;
+    }
+
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
 }

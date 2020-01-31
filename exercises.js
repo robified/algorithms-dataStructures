507924ce05668505d258ac07dd05cc7305fcb6dd
Robin <daorob@gmail.com>
2020-01-31 01:48:27 -0600

    Implement depth first tree traversal method.

--- exercises.js
+++ exercises.js
@@ -1124,4 +1124,14 @@ class Tree {
             fn(node);
         }
     }
+
+    traverseDF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift();
+            
+            arr.unshift(...node.children);
+            fn(node);
+        }
+    }
 }

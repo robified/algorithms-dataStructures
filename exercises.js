f3e46422adae2893686b61bc5a427085104d6f4a
Robin <daorob@gmail.com>
2020-01-31 18:56:58 -0600

    Solve merge function.

--- exercises.js
+++ exercises.js
@@ -1344,5 +1344,15 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
+    const results = [];
 
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
 }
